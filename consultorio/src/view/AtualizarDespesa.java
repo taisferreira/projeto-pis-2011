0af62d8b53fc3d2b14f6e729061040a832174da0
taisborgesferreira@gmail.com <taisborgesferreira@gmail.com@a66de863-8320-19f2-d150-121d0ec85206>
2011-12-08 19:19:28 +0000

    Verificacao de CPF já cadastrado em Cadastrar->Paciente,Funcionario,Medico

--- consultorio/src/view/AtualizarDespesa.java
+++ consultorio/src/view/AtualizarDespesa.java
@@ -47,17 +47,7 @@ public class AtualizarDespesa extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    /**
-    * @param args the command line arguments
-    */
-    public static void main(String args[]) {
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new AtualizarDespesa().setVisible(true);
-            }
-        });
-    }
-
+ 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     // End of variables declaration//GEN-END:variables
 
